e569c6bceae8c78f48ea5fbacba4a319db633d67
Ivan Leo <ivan.itugu@strathmore.edu>
2026-06-19 22:29:57 +0300

    added home page with its exclusive css file

---
+++ react-typescript/src/App.tsx
@@ -0,0 +1,11 @@
+import Greetings from "./Components/Content";
+
+function App() 
+  {
+    return (
+      <>
+        <Greetings />
+      </>
+    );
+  }
+  export default App;
---
+++ react-typescript/src/Components/Content.tsx
@@ -0,0 +1,6 @@
+function Greetings() 
+{ 
+     const name = "Ivan";
+     return <h1>Hello, {name}!</h1>;
+}
+export default Greetings;
